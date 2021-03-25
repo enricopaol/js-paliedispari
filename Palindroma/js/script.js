2916eaa2624007b5208071ae39c1157a2fb9f453
Enrico Paolazzi <paolazzienrico@gmail.com>
2021-03-25 16:38:13 +0100

    Terminato esercizio parola palindroma

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -1,2 +1,43 @@
 // Chiedere all’utente di inserire una parola Creare una 
 // funzione per capire se la parola inserita è palindroma
+
+// Chiedo all'utente di inserire una parola
+var parolaUtente = prompt('Dimmi una parola e ti dirò se è palindroma');
+//console.log(parolaUtente);
+
+// Attivo la funzione isPalindroma
+var message;
+if(isPalindroma(parolaUtente) === true) {
+    message = 'La parola ' + parolaUtente + ' è palindroma.';
+} else {
+    message = 'La parola ' + parolaUtente + ' non è palindroma.'
+}
+
+//stampo il messaggio finale
+alert(message);
+
+
+
+// Riceve una parola (stringa)
+//
+// Legge la parola in entrambi i versi
+//
+// Se è uguale ritorna True, altrimenti False
+function isPalindroma(parolaInserita) {
+    var isParolaPalindroma = false;
+    //console.log('Dentro alla funzione:', parolaInserita);
+
+    // Scorro la parola al contrario e pusho le lettere in un array
+    var parolaReverse = '';
+    for (var i = parolaInserita.length - 1; i >= 0; i--) {
+        var thisLettera = parolaInserita[i];
+        
+       parolaReverse = parolaReverse + thisLettera;
+    }
+
+    if (parolaReverse === parolaInserita) {
+        isParolaPalindroma = true;
+    }
+
+    return isParolaPalindroma
+}
